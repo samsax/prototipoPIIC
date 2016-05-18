c3b74843028de8a0bed259f1293dd0f6fc4811e4
samsax <samuelsaxofon@gmail.com>
2016-05-18 15:56:19 -0500

    camara con movimiento segun giroscopio

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+
+    public float playerMovSpeed;
+    public GameObject head;
+    private bool playerWantsToMove;
+    private Vector3 playerMovDir;
+
+    void Start()
+    {
+        playerWantsToMove = false;
+    }
+
+    void Update()
+    {
+        if (Cardboard.SDK.VRModeEnabled && Cardboard.SDK.Triggered)
+        {
+            playerWantsToMove = !playerWantsToMove;
+        }
+        if (playerWantsToMove == true)
+        {
+            movePlayer();
+        }
+    }
+
+    public void movePlayer()
+    {
+        playerMovDir = new Vector3(transform.forward.x, Mathf.Clamp(transform.forward.y, 0, 0), transform.forward.z);
+        transform.Translate(playerMovDir * playerMovSpeed * Time.deltaTime);
+    }
+}
