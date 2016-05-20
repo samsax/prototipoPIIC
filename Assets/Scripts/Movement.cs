a8ae187066caf5f0b4981ab983f8ba4a6679cef4
saureth <aerials_001@hotmail.com>
2016-05-19 20:57:50 -0500

    arreglado problema movimiento

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,34 +2,29 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Movement : MonoBehaviour
-{
+public class Movement : MonoBehaviour {
 
-    public float playerMovSpeed;
-    public GameObject head;
-    private bool playerWantsToMove;
-    private Vector3 playerMovDir;
+	public float playerMovSpeed;
+	public GameObject head;
+	private bool playerWantsToMove;
+	private Vector3 playerMovDir;
 
-    void Start()
-    {
-        playerWantsToMove = false;
-    }
+	void Start () {
+		playerWantsToMove = false;
+	}
 
-    void Update()
-    {
-        if (Cardboard.SDK.VRModeEnabled && Cardboard.SDK.Triggered)
-        {
-            playerWantsToMove = !playerWantsToMove;
-        }
-        if (playerWantsToMove == true)
-        {
-            movePlayer();
-        }
-    }
+	void Update () {
+		if (Cardboard.SDK.VRModeEnabled && Cardboard.SDK.Triggered) {
+			playerWantsToMove = !playerWantsToMove;
+		}
+		if (playerWantsToMove == true) {
+			movePlayer();
+		}
+	}
 
-    public void movePlayer()
-    {
-        playerMovDir = new Vector3(transform.forward.x, Mathf.Clamp(transform.forward.y, 0, 0), transform.forward.z);
-        transform.Translate(playerMovDir * playerMovSpeed * Time.deltaTime);
-    }
+	public void movePlayer(){
+		playerMovDir = new Vector3(-head.transform.forward.x,Mathf.Clamp(head.transform.forward.y,0,0),-head.transform.forward.z);
+		transform.Translate(playerMovDir*playerMovSpeed*Time.deltaTime);
+	}
+
 }
